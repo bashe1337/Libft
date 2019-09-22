72dd62a3afe5da11d5e45da8883b73d612349c5e
Bess Ashe <bashe@ox-u6.21-school.ru>
2019-09-22 17:57:27 +0300

    bruh

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: bashe <bashe@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/17 19:07:32 by bashe             #+#    #+#             */
-/*   Updated: 2019/09/21 17:57:10 by bashe            ###   ########.fr       */
+/*   Updated: 2019/09/22 17:23:39 by bashe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,21 +36,11 @@ int		ft_wordcounter(char const *s, char c)
 int		ft_wl(char const *s, char c)
 {
 	int		i;
-	int		n;
 
 	i = 0;
-	n = 0;
-	while (s[i])
-	{
-		if (s[i] == c)
-			i++;
-		else
-		{
-			n++;
-			i++;
-		}
-	}
-	return (n);
+	while (s[i] != c && s[i])
+		i++;
+	return (i);
 }
 
 char	**ft_strsplit(char const *s, char c)
@@ -63,16 +53,17 @@ char	**ft_strsplit(char const *s, char c)
 	i = 0;
 	a = 0;
 	b = 0;
-	if (!(str = (char **)malloc(sizeof(char) * ft_wordcounter(s, c) + 1)))
+	str = (char **)malloc(sizeof(char *) * ft_wordcounter(s, c) + 1);
+	if (str == NULL || !s || !c)
 		return (NULL);
 	while (a < ft_wordcounter(s, c))
 	{
+		b = 0;
 		while (s[i] == c && s[i])
 			i++;
 		if (!(str[a] = (char *)malloc(sizeof(char) * ft_wl(&s[i], c) + 1)))
 			return (NULL);
-		b = 0;
-		while (s[i] && s[i] != c)
+		while (s[i] != c && s[i])
 			str[a][b++] = s[i++];
 		str[a][b] = '\0';
 		a++;
